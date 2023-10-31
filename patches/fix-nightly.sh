3fa857b463d6e6f02d4b8d72b74e0fddab817bf4
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2023-10-31 09:36:28 +0000

    Test nightly with 1.75-dev

--- patches/fix-nightly.sh
+++ patches/fix-nightly.sh
@@ -2,12 +2,6 @@
 
 . "$HOME/.cargo/env"
 
-cd /build/rust
-echo "Forcing global workspace libc to run at 0.2.143"
-cargo update -p libc --precise 0.2.143
-
-cd src/tools/cargo
-echo "Forcing cargo workspace libc to run at 0.2.143"
-cargo update -p libc --precise 0.2.143
+cd /build/rust/src/tools/cargo
 echo "Forcing cargo workspace socket2 to downgrade to 0.4.1"
 cargo update -p socket2 --precise 0.4.1
